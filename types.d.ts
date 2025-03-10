80a03f07c4b37773b65702e7de62847b6a0f56cd
JAN JANIAK <janek.j@survicate.com>
2025-03-10 11:01:56 +0100

    Feat-Add-disableSensitiveDataPersistence-key-to-config

--- types.d.ts
+++ types.d.ts
@@ -1,24 +1,34 @@
 import type { ApiEvent, AppearMethod } from './widget_wrapper';
 
+export enum SurveyType {
+  WidgetSurvey = 'WidgetSurvey',
+  PageSurvey = 'PageSurvey',
+  MobileSurvey = 'MobileSurvey',
+  FeedbackButton = 'FeedbackButton',
+  IntercomSurvey = 'IntercomSurvey'
+}
+
 export interface ConfigModel {
-	workspaceKey: string;
-	traits?: {
-	[key: string]: string;
-	};
-	disableTargeting?: true;
+  workspaceKey: string;
+  traits?: {
+  [key: string]: string;
+  };
+  disableTargeting?: true;
+  disableSensitiveDataPersistence?: boolean;
+  nonce?: string;
 }
 
 export interface ShowSurveyOptions {
-	forceDisplay?: true;
-	displayMethod?: AppearMethod;
-	displayOptions?: {
-	delay?: number;
-	scrolledPercentage?: number;
-	};
+  forceDisplay?: true;
+  displayMethod?: AppearMethod;
+  displayOptions?: {
+  delay?: number;
+  scrolledPercentage?: number;
+  };
 }
 
 export interface VisitorAttributes {
-	[key: string]: string | number | boolean | Date;
+  [key: string]: string | number | boolean | Date;
 }
 
 export type CallbackType = (surveyId: string, answer?: unknown) => void;
@@ -26,20 +36,16 @@ export type QuestionAnsweredCallback = (surveyId: string, questionId: number, an
 
 export type CallbackTypes = CallbackType | QuestionAnsweredCallback;
 export declare class Survicate {
-	constructor(config: ConfigModel);
-	private initScript;
-	private loadScript;
-	ready(): Promise<void>;
-	showSurvey(id: string, options?: ShowSurveyOptions): boolean;
-	getSurvey(): {
-			id: string | null;
-			name: string | null;
-	};
-	retarget(): void;
-	getVisitorId(): string;
-	setVisitorTraits(attributes: VisitorAttributes): void;
-	destroyVisitor(callback?: () => void): void;
-	addEventListener(event: ApiEvent, callback: CallbackTypes): number | void;
-	removeEventListener(eventId: number | ApiEvent): void;
-	invokeEvent: (eventName: string, eventProperties?: Record<string, any>) => void;
+  constructor(config: ConfigModel);
+  private initScript;
+  private loadScript;
+  ready(): Promise<void>;
+  showSurvey(id: string, options?: ShowSurveyOptions): boolean;
+  retarget(): void;
+  getVisitorId: (surveyType?: SurveyType) => string;
+  setVisitorTraits(attributes: VisitorAttributes): void;
+  destroyVisitor(callback?: () => void): void;
+  addEventListener(event: ApiEvent, callback: CallbackTypes): number | void;
+  removeEventListener(eventId: number | ApiEvent): void;
+  invokeEvent: (eventName: string, eventProperties?: Record<string, any>) => void;
 }
